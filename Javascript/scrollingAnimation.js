54f75094da1497cb51ef1bbfa29b2db7bfe06ae2
Jesse Phillips <Jess@Aikos-MacBook-Pro.local>
2016-08-09 00:35:51 -0600

    Not to keep from hanging.

--- Javascript/scrollingAnimation.js
+++ Javascript/scrollingAnimation.js
@@ -138,13 +138,13 @@ function CheckEachBox(){
 
     for (var i = 0; i < MidElements.length; i++) {
 
-        if(MidElements[i]==null){return}else{
+        if(MidElements[i]!=null){
 
             if(MidElements[i].offsetTop>window.innerHeight*.5){
            MidElements[i].className="movingBoxLow"
             }
 
-             if(MidElements[i].offsetTop<window.innerHeight*.5){
+             if(MidElements[i].offsetTop<0){
            MidElements[i].className="movingBoxHigh"
             }
 
