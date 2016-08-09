baeee629543464b4234fe772a46c918d70d74064
Jesse Phillips <Jess@Aikos-MacBook-Pro.local>
2016-08-08 21:57:36 -0600

    Basic border thickness script created and linked
    should function basically now.

--- Javascript/BorderAnimation.js
+++ Javascript/BorderAnimation.js
@@ -1,13 +1,10 @@
 
 
-var $maxBorderThickness=5;
-var $minBorderTHickness=1;
-
 var MovingBoxElement = document.getElementsByClassName('movingBox')
 
 
 //Call each element of the class and pass to the animation function
-function CheckEach(){
+function CheckEachBox(){
 
 	for (i = 0; i < MovingBoxElement.length; i++) {
 
@@ -15,3 +12,34 @@ function CheckEach(){
     }
 }
 
+
+function updateboxes(ThisBox){
+
+	var Top=ThisBox.offsetTop;
+	var Height=ThisBox.height;
+	var Bottom=Top-Height;
+	var WindowHeight=window.innerHeight;
+//check top location
+	if (Top>(WindowHeight*.5)){
+		//above half way set to minimum
+		ThisBox.style.borderTopWidth=1
+
+	}else{
+       //adjust Thickness
+       ThisBox.style.borderTopWidth=5 //temp for debug test
+		
+	}
+//check bottom location and asjust
+		if (Bottom>(WindowHeight*.5)){
+
+		//adjust Thickness
+		 ThisBox.style.borderTopWidth=5//temp for debug test
+
+	}else{
+       //Below half way set to minimum
+       ThisBox.style.borderTopWidth=1
+		
+	}
+
+
+}
