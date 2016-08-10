892a915fa4ec80b66ffb3db0b35ad098085e5f55
Jesse Phillips <Jess@Aikos-MacBook-Pro.local>
2016-08-10 16:47:30 -0600

    Working now for about. Time to move to bechance api.
    This update is just cleanup. Moving the toggle script to another file.
    Later can animate if desired.

---
+++ Javascript/ToggleVisibility.js
@@ -0,0 +1,11 @@
+ <script>
+ function toggleAbout(PassedID) {
+    var x = document.getElementById(PassedID);
+    if(x.style.display=="none" || x.style.display=="null" || x.style.display==""){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+    
+ }
+</script>
